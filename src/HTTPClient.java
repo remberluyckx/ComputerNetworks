c34f14e9a8a562adeb27acf5ad5fda81f7561967
rember <remberluyckx@gmail.com>
2018-03-05 16:01:24 +0100

    Initial commit

---
+++ src/HTTPClient.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.net.*;
+
+public class HTTPClient {
+	public static void main(String argv[]) throws Exception
+	 { 
+		System.out.println("1");
+	 BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+	 	System.out.println("2");
+	 Socket clientSocket = new Socket("localhost", 6789);
+	 System.out.println("3");
+	 DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+	 BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+	 String sentence = inFromUser.readLine();
+	 outToServer.writeBytes(sentence + '\n');
+	 String modifiedSentence = inFromServer.readLine();
+	 System.out.println("FROM SERVER: " + modifiedSentence);
+	 clientSocket.close();
+	 }
+}
---
+++ src/HTTPServer.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+
+public class HTTPServer {
+	public static void main(String argv[]) throws Exception
+	 {
+	 ServerSocket welcomeSocket = new ServerSocket(6789);
+	 while(true)
+	 {
+		 Socket connectionSocket = welcomeSocket.accept();
+		 BufferedReader inFromClient = new BufferedReader(new InputStreamReader (connectionSocket.getInputStream()));
+		 DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+		 String clientSentence = inFromClient.readLine();
+		 System.out.println("Received: " + clientSentence);
+		 String capsSentence = clientSentence.toUpperCase() + '\n';
+		 outToClient.writeBytes(capsSentence);
+	 }
+	 }
+}
