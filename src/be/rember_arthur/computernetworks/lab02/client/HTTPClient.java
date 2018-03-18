796725e4119feaa4a7e0bcaa0b717d70dfe3cdce
Unknown <remberluyckx@gmail.com>
2018-03-19 00:08:03 +0100

    GET and HEAD works (with some links)

--- src/be/rember_arthur/computernetworks/lab02/client/HTTPClient.java
+++ src/be/rember_arthur/computernetworks/lab02/client/HTTPClient.java
@@ -25,30 +25,41 @@ public class HTTPClient {
 		 String hostname = url.getHost();
 		 int port = 80;*/
 
-		 try (Socket socket = new Socket("www.google.com", 80)) {
+		 //command, uri
+		 String[] testArg = {"GET", "www.google.be/?gfe_rd=cr&dcr=0&ei=9OOuWtqzFJKq8weHv5_wBw"};
+		 String command = testArg[0];
+		 String Uri = testArg[1];
+		 String hostname = Uri.substring(0, Uri.lastIndexOf(".") + 3); //voor .com, .org moet dit +4 zijn
 
+		 try (Socket socket = new Socket(hostname, 80)) {
+
+		 	 //set up output to server
 			 OutputStream output = socket.getOutputStream();
 			 PrintWriter requestWriter = new PrintWriter(output, true);
 
-			 requestWriter.println("GET " + "http://www.google.be/?gfe_rd=cr&dcr=0&ei=k2uuWrPcHcOh4gTf44_ICQ" + " HTTP/1.1");
-			 requestWriter.println("Host: " + "www.google.com");
+			 //form request
+			 requestWriter.println(command + " " + Uri + " HTTP/1.1");
+			 requestWriter.println("Host: " + hostname);
 			 requestWriter.println("User-Agent: Simple Http Client");
 			 requestWriter.println("Accept: text/html");
 			 requestWriter.println("Accept-Language: en-US");
 			 //requestWriter.println("Connection: close");
 			 requestWriter.println();
 
+			 //get response
 			 InputStream input = socket.getInputStream();
-
 			 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
 			 String line;
-
 			 PrintWriter responseWriter = new PrintWriter("response.html", "UTF-8");
+
+			 int imagesAmount = 0;
 
 			 while ((line = reader.readLine()) != null) {
 				 System.out.println(line);
-				 responseWriter.println(line); //write to html file
+
+				 //write to html file
+				 responseWriter.println(line);
 
 				 //filter all img tags, filter src attribute, get image urls
 				 if (line.contains("<img")) {
@@ -58,48 +69,26 @@ public class HTTPClient {
 					 String srcUrl = srcAttributes[1].substring(0, srcAttributes[1].indexOf("\""));
 					 System.out.println(srcUrl);
 
-					 //OutputStream output2 = socket.getOutputStream();
-					 //PrintWriter requestWriter2 = new PrintWriter(output2, true);
-
-					 requestWriter.println("GET " + "http://www.google.com" + srcUrl + " HTTP/1.1");
-					 requestWriter.println("Host: " + "www.google.com");
+					 //GET images
+					 requestWriter.println(command + " " + hostname + srcUrl + " HTTP/1.1");
+					 requestWriter.println("Host: " + hostname);
 					 requestWriter.println("User-Agent: Simple Http Client");
 					 requestWriter.println("Accept: image/gif");
 					 requestWriter.println("Accept-Language: en-US");
-					 //requestWriter.println("Connection: close"); // comment out after this works
+					 requestWriter.println("Connection: close"); // comment out after this works
 					 requestWriter.println();
 
 					 InputStream imgStream = socket.getInputStream();
 
-					 /*BufferedImage imBuff = ImageIO.read(imgStream);
-
-					 Image img = ImageIO.read(imgStream);
-
-
-
-					 File outputfile = new File("image.png");
-					 ImageIO.write(imBuff, "png", outputfile);*/
-
-
-
-					 File file = new File("image.png");
-
+					 File file = new File("image" + imagesAmount+1 +".png");
 					 OutputStream outStream = new FileOutputStream(file);
 
 					 byte[] buffer = new byte[8 * 1024];
 					 int bytesRead;
 					 while ((bytesRead = imgStream.read(buffer)) != -1) {
-							 outStream.write(buffer, 333, bytesRead);
+							 outStream.write(buffer, 352, bytesRead);
 					 }
-
-
-					 /*BufferedReader reader2 = new BufferedReader(new InputStreamReader(imgStream));
-
-					 String line2;
-
-					 while ((line2 = reader2.readLine()) != null) {
-						 System.out.println("NL" + line2);
-					 }*/
+					imagesAmount++;
 				 }
 
 				 }
